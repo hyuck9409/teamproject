7f9f30f6eb268b9126f5b61bc18d5f677f2de050
hyuck9409 <djfh611@naver.com>
2024-06-03 20:10:37 +0900

    로그인 항상 성공

--- src/main/java/controller/account/accountController.java
+++ src/main/java/controller/account/accountController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import data.dto.UserDto;
 import data.service.UserService;
@@ -33,4 +34,44 @@ public class accountController {
 		userService.insertUser(dto);
 		return "redirect:./login";
 	}
+	
+	 
+	    
+	    @GetMapping("/profile")
+	    public String showProfile() {
+	        return "layout/profile"; // 
+	    }
+	    
+	    @ResponseBody
+		@GetMapping("/login")
+		public Map<String, String> isLogin(
+				@RequestParam(defaultValue = "no") String saveid,/*널값이 넘어올경우 디폴트값이 적용된다*/
+				@RequestParam String myid,
+				@RequestParam String pass,
+				HttpSession session)
+		{
+			System.out.println("saveid="+saveid);
+			Map<String, String> map=new HashMap<>();
+			//로그인 상태
+			boolean loginStatus=userService.isLoginCheck(myid, pass);
+			if(loginStatus) {
+				//아이디와 비번이 맞은경우
+				map.put("status", "success");
+				//로그인 성공시 세션에 저장
+				session.setAttribute("saveid", saveid.equals("no")?"no":"yes");
+				session.setAttribute("loginok", "yes");
+				session.setAttribute("loginid", myid);
+			}else {
+				//아이디와 비번이 틀린경우
+				map.put("status", "fail");
+			}
+			return map;
+		}
+		
+		//로그아웃시 호출
+		@ResponseBody
+		@GetMapping("/logout")
+		public void memberLogout(HttpSession session) {
+			session.removeAttribute("loginok");
+		}
 }
--- src/main/java/data/mapper/UserMapperInter.java
+++ src/main/java/data/mapper/UserMapperInter.java
@@ -1,7 +1,10 @@
 package data.mapper;
 
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import data.dto.UserDto;
 
@@ -12,5 +15,14 @@ public interface UserMapperInter {
 			insert into user (email, password, birthday, nickname) values (#{email},#{password},#{birthday},#{birthday})
 			""")
 	public void insertUser(UserDto dto);
+	
+	@Select("select count(*) from user where num=#{num} and password=#{password}")
+	public int isEqualPassCheck(Map<String, Object> map);
+	
+	@Select("""
+			select count(*) from user where email=#{email} and password=#{password}
+			""")
+	public int isLoginCheck(String email,String password);
 
+	
 }
